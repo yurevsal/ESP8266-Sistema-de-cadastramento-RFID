13e698c01dc2f9a92132b20f5ab921278afcc06a
yurevsal <41169404+yurevsal@users.noreply.github.com>
2022-01-13 10:04:10 -0300

    Update Cadastro_RFID_SPIFFS.ino

--- Cadastro_RFID_SPIFFS/Cadastro_RFID_SPIFFS.ino
+++ Cadastro_RFID_SPIFFS/Cadastro_RFID_SPIFFS.ino
@@ -253,7 +253,7 @@ void loop() {
   if (mfrc522.PICC_ReadCardSerial()) {
     Serial.print("UID da tag :");
     String rfid_data = "";
-    for (byte i = 0; i < mfrc522.uid.size; i++)
+    for (uint8_t i = 0; i < mfrc522.uid.size; i++)
     {
       Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
       Serial.print(mfrc522.uid.uidByte[i], HEX);
